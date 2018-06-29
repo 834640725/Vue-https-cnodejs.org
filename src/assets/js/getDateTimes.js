35ab0225f8b3581163d88fa3d6d7964404d9e22b
qiqingfu <723469354@qq.com>
2018-06-29 16:49:18 +0800

    时间转换

--- src/assets/js/getDateTimes.js
+++ src/assets/js/getDateTimes.js
@@ -2,41 +2,81 @@ import Vue from 'vue'
 let dayjs = require('dayjs');
 dayjs().format();
 
+/**
+ *
+ * @param obj Object
+ * @param key String
+ */
+function convert(obj,key){
+
+    let timesd = obj[key];
+
+    let items = dayjs(timesd).unix();
+    let newTimes = dayjs().unix();  // 系统时间戳
+    let diffTime = newTimes - items;  // 差值时间戳
+    let day = parseInt(diffTime / 86400);
+
+    obj[key] = day;
+    let t = obj[key];
+
+    if(t <= 7 && t >= 1){
+      obj[key] = t + ' 天前'
+    }
+    else if(t > 7 && t < 14){
+      obj[key] = "1 周前"
+    }
+    else if(t >= 14 && t < 21){
+      obj[key] = "2 周前"
+    }
+    // 3周前
+    else if(t >= 21 && t < 28){
+      obj[key] = "3 周前"
+    }
+    //4 周前
+    else if(t >= 28 && t <= 31){
+      obj[key] = "4 周前"
+    }
+
+    else if(t > 31){
+      let dateObj = t;
+      // 获取到 2018/4/10格式的时间
+      let newTimer = new Date(dateObj*1000).toLocaleString().split(' ')[0].split('/').join(' /');
+      obj[key] = newTimer;
+    }
+
+    else if(t >= 0 && t < 1) {
+      // 计算小时 1 <= h < 24
+      let hours = parseInt(diffTime / 3600);
+      if (1 <= hours && hours < 24) {
+        obj[key] = hours + ' 小时前'
+      }
+
+      if( 0 <= hours && hours < 1) {
+        let minutes = parseInt(diffTime / 60);
+        obj[key] = minutes + ' 分钟前'
+
+        if(minutes < 1){
+          obj[key] = diffTime + ' 秒前'
+        }
+      }
+    }
+}
+
+
+
 export function getDateTimes(data){
   let datalist = data.data;
 
+  if(!Array.isArray(datalist)){
+    convert(datalist,'create_at');
+    return;
+  }
+
+  // 转换时间格式数据为数组
   datalist.forEach(item => {
-
-    let items = dayjs(item.last_reply_at).unix();  // 发布时间戳
-    let newTimes = dayjs().unix();  // 系统时间戳
-
-    let diffday = newTimes - items;  // 差值时间戳
-
-    item.last_reply_at = diffday;
-
-    /*
-    * 1分钟 60s
-    * 1小时 3600s
-    * 1天   43200s
-    * 1周   3024000
-    * */
-    let t = item.last_reply_at;
-    if(t < 60){
-      item.last_reply_at = t + ' 秒前'
-    }
-
-    else if(60 <= t && t < 3600){
-      item.last_reply_at = Math.floor(t / 60) + ' 分钟前'
-    }
-
-    else if(3600 <= t && t < 43200){
-      item.last_reply_at = Math.floor(t / 3600) + ' 小时前'
-    }
-
-    else if(43200 <= t && t < 3024000){
-      item.last_reply_at = Math.floor(t / 43200) + ' 天前'
-    }
+    convert(item,'last_reply_at')
   })
+
 
 }
 
--- src/components/details/details.vue
+++ src/components/details/details.vue
@@ -8,18 +8,21 @@
               <!--<span class="det-title">【头条】第六届 杭州 Node Party 杭电专场开始报名啦</span>-->
               <span class="det-title">{{userInfo.title}}</span>
               <div class="header-info">
-                <span>● 发布于 17 天前</span>
-                <span>● 作者: <a href="#" target="_blank">xinyu12345</a></span>
-                <span>● 3591 次浏览</span>
-                <span>● 来自 分享</span>
+                <span>● 发布于 {{userInfo.create_at}}</span>
+                <span v-if="userInfo.author">
+                  ● 作者: <a href="#" target="_blank">{{userInfo.author['loginname']}}</a>
+                </span>
+                <span>● {{userInfo.visit_count}} 次浏览</span>
+                <span>● 来自 {{userInfo.tab}}</span>
               </div>
             </header>
 
             <div class="article">
-              <div class="markdown-text" v-html="userInfo.content">
-
-              </div>
+              <div class="markdown-text" v-html="userInfo.content"></div>
             </div>
+
+            <!--评论列表-->
+
           </div>
 
           <div class="section-right">
@@ -40,6 +43,9 @@
 </template>
 
 <script>
+
+  import {getDateTimes} from '@/assets/js/getDateTimes.js'
+
     export default {
       data(){
         return{
@@ -55,8 +61,8 @@
          };
          if(id){
            this.http.getTopicId(params).then(({data}) => {
+              getDateTimes(data);
               this.userInfo = data.data;
-              console.log(this.userInfo)
            })
          }else{
            this.$router.push({path:'/index'})
